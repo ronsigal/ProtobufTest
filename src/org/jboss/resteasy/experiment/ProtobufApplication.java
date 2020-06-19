7af771f370138b7d39389c769cb081cdfc2eeb0b
Ron Sigal <rsigal@redhat.com>
2020-06-19 11:46:09 -0400

    Added ProtobufProvider to Application

--- src/org/jboss/resteasy/experiment/ProtobufApplication.java
+++ src/org/jboss/resteasy/experiment/ProtobufApplication.java
@@ -5,12 +5,15 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import org.jboss.resteasy.plugins.protobuf.ProtobufProvider;
+
 public class ProtobufApplication extends Application
 {
    public Set<Class<?>> getClasses() {
       Set<Class<?>> classes = new HashSet<Class<?>>();
       classes.add(ProtobufWFResource.class);
       classes.add(JsonBindingProviderExp.class);
+      classes.add(ProtobufProvider.class);
       return classes;
   }
 }
