90bb14903e4139efbfd621e75ad6bcdba72a5004
Ron Sigal <rsigal@redhat.com>
2020-06-20 14:53:09 -0400

    List /opt/eap

--- src/org/jboss/resteasy/plugins/protobuf/ProtobufCompiler.java
+++ src/org/jboss/resteasy/plugins/protobuf/ProtobufCompiler.java
@@ -175,17 +175,17 @@ public class ProtobufCompiler
    private static void compileProtoDescriptor(String directory, String protoFilename) throws IOException, InterruptedException
    {
       System.out.println("jboss.home.dir: " + System.getProperty("jboss.home.dir "));
-//      File f = new File(System.getProperty("/opt/eap"));
-//      if (f != null)
-//      {
-//         String[] pathnames = f.list();
-//
-//         // For each pathname in the pathnames array
-//         for (String pathname : pathnames) {
-//             // Print the names of files and directories
-//             System.out.println(pathname);
-//         }
-//      }
+      File f = new File("/opt/eap");
+      if (f != null)
+      {
+         String[] pathnames = f.list();
+
+         // For each pathname in the pathnames array
+         for (String pathname : pathnames) {
+             // Print the names of files and directories
+             System.out.println(pathname);
+         }
+      }
       System.out.println("user.dir: " + System.getProperty("user.dir"));
       System.out.println("JBOSS_HOME: " + System.getenv("$JBOSS_HOME"));
       System.out.println("jboss.home.dir: " + System.getProperty("jboss.home.dir"));
